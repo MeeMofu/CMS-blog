2a66dd51361b35c4f1a4e701123bd2c821b89ed5
VTran <MeeMofu@users.noreply.github.com>
2020-11-12 11:48:34 -0500

    Added edit function

--- public/javascript/dashboard.js
+++ public/javascript/dashboard.js
@@ -1,8 +1,8 @@
 async function newpost(event) {
     event.preventDefault();
   
-    const title = $('#title').val();
-    const content = $('#content').val();
+    const title = $('#title').val().trim();
+    const content = $('#content').val().trim();
   
     const response = await fetch(`/api/posts`, {
       method: 'POST',
@@ -21,16 +21,52 @@ async function newpost(event) {
       alert(response.statusText);
     }
   }
+async function deletepost(event) {
+    const id = $(this).attr('data-post')
+    const response = await fetch(`/api/posts/${id}`, {
+        method: 'DELETE'
+    });
+
+    if (response.ok) {
+        document.location.reload();
+    } else {
+        alert(response.statusText);
+    }
+}
+
+async function updatePost(event) {
+    const content = $('textarea[data-post="'+$(this).attr('data-post')+'"]').val();
+    const id = $(this).attr('data-post');
+    const response = await fetch(`/api/posts/${id}`, {
+      method: 'PUT',
+      body: JSON.stringify({
+        content
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+  
+    if (response.ok) {
+      document.location.reload();
+    } else {
+      alert(response.statusText);
+    }
+  }
 
 $('#new-post').click(()=>{
     $('#new-post').hide();
     $('#new-field').show();
-})
+});
 
 $('.edit').click(function(event){
     // console.log($(this).attr('data-post'));
     $('.edit[data-post="'+$(this).attr('data-post')+'"]').hide();
     $('.card-body[data-post="'+$(this).attr('data-post')+'"]').show();
-})
+});
 
-$('#publish').click(newpost)
+$('#publish').click(newpost);
+
+$('.delete').click(deletepost);
+
+$('.update').click(updatePost);
